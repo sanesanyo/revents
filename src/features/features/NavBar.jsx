d5153508451ed9c95c8241e7272809465b440c16
Sanyog Yadav <sanesanyo@gmail.com>
2022-01-31 14:29:19 +0100

    End of Section 3

---
+++ src/features/features/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Menu } from 'semantic-ui-react'
+
+export default function NavBar() {
+    return (
+        <Menu inverted fixed='top'>
+<Container>
+    
+</Container>
+        </Menu>
+    )
+}
